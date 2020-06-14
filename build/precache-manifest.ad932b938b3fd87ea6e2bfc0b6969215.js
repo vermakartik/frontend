5b84815ca0e3348ceb77a91522907e7160f67ba4
Kartik Verma <kvs1297@gmail.com>
2020-06-14 19:22:20 +0530

    adde build

---
+++ build/precache-manifest.ad932b938b3fd87ea6e2bfc0b6969215.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdd1e6c52e94ef55f92fcf58fc4965b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b8f534790bc8fdcdf0",
+    "url": "/static/css/main.49811a14.chunk.css"
+  },
+  {
+    "revision": "9174a3e7d354df15c23c",
+    "url": "/static/js/2.4ee5359e.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/static/js/2.4ee5359e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b8f534790bc8fdcdf0",
+    "url": "/static/js/main.2c374baa.chunk.js"
+  },
+  {
+    "revision": "04a2a5a74c6f11dfcb08",
+    "url": "/static/js/runtime-main.ee243ce0.js"
+  },
+  {
+    "revision": "d2a44995526e6bf52b6468a516fbc7d8",
+    "url": "/static/media/DigitRecognitionCNNImageMain.d2a44995.png"
+  },
+  {
+    "revision": "77fa00996ecb4104c7880b8749c7c4e0",
+    "url": "/static/media/Righteous-Regular.77fa0099.ttf"
+  },
+  {
+    "revision": "df2e702629c1a73e440349c602142828",
+    "url": "/static/media/android.df2e7026.png"
+  },
+  {
+    "revision": "4af8d68db8387c70d41d37a8ccd3e753",
+    "url": "/static/media/apollo.4af8d68d.png"
+  },
+  {
+    "revision": "da0bdea391b0359ad62b20e93e378c5a",
+    "url": "/static/media/ar_chess.da0bdea3.png"
+  },
+  {
+    "revision": "44b223ad896c78b16682dcf37a6bb6af",
+    "url": "/static/media/archia-regular-webfont.44b223ad.ttf"
+  },
+  {
+    "revision": "3e1376d9d939f97537452217c509e206",
+    "url": "/static/media/arsolarsystem.3e1376d9.png"
+  },
+  {
+    "revision": "b9ad4bb9e03980ad8c8e09021c32714a",
+    "url": "/static/media/blender.b9ad4bb9.png"
+  },
+  {
+    "revision": "9bde80c47d26ad9ad1cc777b039fe42f",
+    "url": "/static/media/c_plus_plus.9bde80c4.png"
+  },
+  {
+    "revision": "562eee740b5e72b1f51605745326360e",
+    "url": "/static/media/c_program.562eee74.png"
+  },
+  {
+    "revision": "0fc7b60227f7416fee18956dd3747d8d",
+    "url": "/static/media/csaigmc.0fc7b602.jpg"
+  },
+  {
+    "revision": "2278354536254adf1cf8375a63539603",
+    "url": "/static/media/csharp.22783545.png"
+  },
+  {
+    "revision": "9c1431e38a3e9d8b5bd6af8d7001aaf5",
+    "url": "/static/media/css3.9c1431e3.png"
+  },
+  {
+    "revision": "7da6dc9fed45209fea41b642662d0b18",
+    "url": "/static/media/edito.7da6dc9f.JPG"
+  },
+  {
+    "revision": "1da029b3c1a275f00a75e7bb3ab8d9db",
+    "url": "/static/media/firebase.1da029b3.png"
+  },
+  {
+    "revision": "36929981c9b87a1a93b946382d2ad20b",
+    "url": "/static/media/graphql.36929981.png"
+  },
+  {
+    "revision": "e463a4b4d36965f50e0556abe4ca8475",
+    "url": "/static/media/graphy.e463a4b4.png"
+  },
+  {
+    "revision": "bd006f80aae86be0e897d5c3192ec311",
+    "url": "/static/media/guiTerm.bd006f80.JPG"
+  },
+  {
+    "revision": "7dbe2981c2991549aff8181aab1f1c12",
+    "url": "/static/media/hillshare.7dbe2981.JPG"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/static/media/html.2ea01c0e.png"
+  },
+  {
+    "revision": "a37eeddf89e5a9ea840cf301858d2513",
+    "url": "/static/media/javascript.a37eeddf.png"
+  },
+  {
+    "revision": "e8644c9b9458991592dcb686dbff3700",
+    "url": "/static/media/leadership.e8644c9b.png"
+  },
+  {
+    "revision": "338ad539ae5683a0bfdc236e4c9e2783",
+    "url": "/static/media/linux.338ad539.ico"
+  },
+  {
+    "revision": "17971e62f5dc6c3d50ede81e41ac0d86",
+    "url": "/static/media/magnetGame.17971e62.JPG"
+  },
+  {
+    "revision": "009df475e02ebc2159d318b58e70ab4b",
+    "url": "/static/media/moneygame.009df475.JPG"
+  },
+  {
+    "revision": "68821a503081f6c4f8b0afb5dfcd40fa",
+    "url": "/static/media/mongoose.68821a50.png"
+  },
+  {
+    "revision": "1a5736ae938fee6f70322bc868c91143",
+    "url": "/static/media/mysql.1a5736ae.png"
+  },
+  {
+    "revision": "ca72ddf798ec156f3765ee7ab43cf00b",
+    "url": "/static/media/neuralStyleTransfer.ca72ddf7.JPG"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/static/media/nodejs.f8dab57d.png"
+  },
+  {
+    "revision": "b91e971ff2b02301ca14f3d9496edceb",
+    "url": "/static/media/plax_project.b91e971f.png"
+  },
+  {
+    "revision": "476aa50ea5c9d48dd9521188957b26a7",
+    "url": "/static/media/profo.476aa50e.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "3ac2e2a473fd81f260ecae0a5a3e4c88",
+    "url": "/static/media/resistanceGame.3ac2e2a4.JPG"
+  },
+  {
+    "revision": "bf151eeb8b27d79149353d40c247d81b",
+    "url": "/static/media/ruby.bf151eeb.png"
+  },
+  {
+    "revision": "9cb75c733eb486d363471d8b8277ad04",
+    "url": "/static/media/snakky.9cb75c73.JPG"
+  },
+  {
+    "revision": "d8cb6c247f153aea0c0b970f0d50a14d",
+    "url": "/static/media/xml.d8cb6c24.png"
+  }
+]);
